8f37ee0293bcec115713253a0add8bc7f9655d4b
Jen Wariner <jenniferwariner@yahoo.com>
2023-07-17 17:16:02 -0500

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+
+    return (
+        <div>
+            <a href="https://github.com/JenWariner19">GitHub</a>
+            <a href="https://www.linkedin.com/in/jennifer-wariner-aaa496a/">LinkedIn</a>
+            <a href="https://stackoverflow.com/users/22242816/jenwariner19">Stack Overflow</a>
+        </div>
+    );
+}
